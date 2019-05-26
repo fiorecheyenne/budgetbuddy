496c22bbc570679a4743a2bef02bb8c4364f6874
Daniel Bryant <azbryant85@gmail.com>
2019-05-26 11:40:38 -0700

    Added basic user.js to models, changed html routing for landing page to '/', added db to schema.sql in the models folder.

--- project2/routes/htmlRoutes.js
+++ project2/routes/htmlRoutes.js
@@ -1,11 +1,14 @@
-var db = require("../models");
-
 module.exports = function(app) {
   app.get("/table", function(req, res) {
     res.render("table");
   });
 
-  app.get("/index", function(req, res) {
+  // THE FOLLOWING .GET DOES THE SAME THING EXCEPT YOU DONT NEED TO TYPE /TABLE TO GET TO THE HOME PAGE.
+  // app.get("/index", function(req, res) {
+  //   res.render("index");
+  // });
+
+  app.get("/", function(req, res) {
     res.render("index");
   });
 
