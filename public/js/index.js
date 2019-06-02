31d6417a5595d43305c51af5122363610c92f08c
fioriecheyenne <fioriecheyenne@gmail.com>
2019-06-02 00:40:11 -0700

    added budget breakdown page, started chart display, added placeent for user info and income

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 
+
 $("#new").on("click", () => {
   event.preventDefault();
   $("#newusername").addClass("is-active");
@@ -39,3 +40,46 @@ $("#add").on("click", () => {
    newRow.clone().appendTo(".is-half").removeClass("erow");
   }
 );
+
+$("#budgetbreak").on("click", () => {
+  window.location.href="./pages/userbreakdown.html"
+});
+
+new Chart(document.getElementById("doughnut-chart"), {
+  type: 'doughnut',
+  data: {
+    labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+    datasets: [
+      {
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: [2478,5267,734,784,433]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Predicted world population (millions) in 2050'
+    }
+  }
+});
+
+// var ctx = $('#expChart');
+// var expChart = new Chart(ctx, {
+//   type: 'doughnut',
+//   data: {
+//     labels: ['Transportation', 'Medical', 'Loans', 'Rent', 'Utilities', 'Entertainment', 'Groceries', 'Personal Care', 'Savings'],
+//     datasets: [{
+//       label: "By category",
+//       backgroundColor: ["#c58ee5", "#8ed9e5", "#8ee5c5", "#e88b9c", "#eaedb1", "#efd1a7","#d18ebe", "#659dce", "#d7bfef"],
+//       data: [14, 45, 12, 20, 9]
+//     }]
+//   },
+//   options: {
+//     title: {
+//       display: true,
+//       text: 'Expenses by category'
+//     }
+//   }
+// });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,9 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, "/../public/pages/index.html"));
   });
 
+  app.get("/userbreakdown", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../public/pages/userbreakdown.html"));
+  });
   // Render 404 page for any unmatched routes
   app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/../public/pages/404.html"));
