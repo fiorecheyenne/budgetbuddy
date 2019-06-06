194e3ffe9b3d1991c491620da00f861dd6ffe742
Daniel Bryant <azbryant85@gmail.com>
2019-06-06 07:42:36 -0700

    added GET to breakdown page

--- public/js/userbreakdown.js
+++ public/js/userbreakdown.js
@@ -1,123 +1,175 @@
 
 //user breakdown page javascript
 
+$(document).ready(function () {
+
+  var url = window.location.search;
+  var idHolder;
+  var userID;
+  var userData = [];
+
+  function grabID() {
+
+    if (url.indexOf("?userID=") !== -1) {
+      idHolder = url.split("=")[1];
+    }
+
+    // Checking to make sure we got the user ID
+    // console.log(idHolder);
+
+    //Changing ID into and Int
+    userID = parseInt(idHolder);
+
+    //Testing to make sure that the id is not a string
+    // if (Number.isInteger(userID)) {
+    //   console.log("IS")
+    // }
+    // else {
+    //   console.log("is not")
+    // }
+
+    userDataLoad()
+
+  }
+
+  function userDataLoad() {
+
+    // console.log(userID);
+
+    $.get("/api/budgets", function (data) {
+      // console.log(data);
+
+      for (var i = 0; i < data.length; i++) {
+
+        if (data[i].UserId === userID) {
+          // console.log(data[i]);
+
+          userData.push(data[i]);
+        }
+      }
+    });
+    console.log(userData);
+  }
+
+  grabID();
+});
 
 //function for chart display
 var options = {
-    chart: {
-      type: 'donut',
-      height: 300
+  chart: {
+    type: 'donut',
+    height: 300
+  },
+  series: [44, 55, 13, 33],
+  labels: ['Transportation', 'Medical', 'Loans', 'Rent', 'Utilities', 'Entertainment', 'Groceries', 'Personal Care', 'Savings'],
+  dataLabels: {
+    enabled: false,
+    formatter: function (val) {
+      return val + "%"
     },
-    series: [44, 55, 13, 33],
-    labels: ['Transportation', 'Medical', 'Loans', 'Rent', 'Utilities', 'Entertainment', 'Groceries', 'Personal Care', 'Savings'],
-    dataLabels: {
-      enabled: false,
-      formatter: function (val) {
-        return val + "%"
+  },
+  legend: {
+    show: false
+  },
+  plotOptions: {
+    pie: {
+      size: 100
+    },
+    donut: {
+      size: '80%'
+    },
+  },
+};
+const chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+//Expenses modal triggers 
+const expenseModal = $("#expensesmodal");
+//Toggle modal on button click
+$("#editexp").on("click", () => {
+  event.preventDefault();
+  expenseModal.addClass("is-active");
+});
+//Close modal when x is clicked
+$("#closeEmodal").on("click", () => {
+  expenseModal.removeClass("is-active");
+});
+//Close modal when cancel is clicked
+$("#cancelEchange").on("click", () => {
+  expenseModal.removeClass("is-active");
+});
+
+//Income modal triggers
+const incomeModal = $("#incomemodal");
+//Toggle modal on click
+$("#editinc").on("click", () => {
+  incomeModal.addClass("is-active");
+});
+//Close modal when x is clicked
+$("#closeImodal").on("click", () => {
+  incomeModal.removeClass("is-active");
+})
+//Close modal when cancel is clicked
+$("#cancelIchange").on("click", () => {
+  incomeModal.removeClass("is-active");
+})
+
+
+
+
+var options = {
+  chart: {
+    height: 280,
+    type: "radialBar",
+  },
+
+  series: [67],
+  colors: ["#20E647"],
+  plotOptions: {
+    radialBar: {
+      hollow: {
+        margin: 0,
+        size: "70%",
+        background: "#293450"
       },
-    },
-    legend: {
-      show: false
-    },
-      plotOptions: {
-        pie: {
-          size: 100
+      track: {
+        dropShadow: {
+          enabled: true,
+          top: 2,
+          left: 0,
+          blur: 4,
+          opacity: 0.15
+        }
+      },
+      dataLabels: {
+        name: {
+          offsetY: -10,
+          color: "#fff",
+          fontSize: "13px"
         },
-          donut: {
-            size: '80%'
-          },
-    },
-  };
-  const chart = new ApexCharts(document.querySelector("#chart"), options);
-  chart.render();
-  
-  //Expenses modal triggers 
-  const expenseModal = $("#expensesmodal");
-  //Toggle modal on button click
-  $("#editexp").on("click", () => {
-    event.preventDefault();
-    expenseModal.addClass("is-active");
-  });
-  //Close modal when x is clicked
-  $("#closeEmodal").on("click", () => {
-    expenseModal.removeClass("is-active");
-  });
-  //Close modal when cancel is clicked
-  $("#cancelEchange").on("click", () => {
-    expenseModal.removeClass("is-active");
-  });
-  
-    //Income modal triggers
-    const incomeModal = $("#incomemodal");
-    //Toggle modal on click
-    $("#editinc").on("click", () => {
-      incomeModal.addClass("is-active");
-    });
-    //Close modal when x is clicked
-    $("#closeImodal").on("click", () => {
-      incomeModal.removeClass("is-active");
-    })
-    //Close modal when cancel is clicked
-    $("#cancelIchange").on("click", () => {
-      incomeModal.removeClass("is-active");
-    })
-  
-  
-  
-  
-    var options = {
-      chart: {
-        height: 280,
-        type: "radialBar",
-      },
-    
-      series: [67],
-      colors: ["#20E647"],
-      plotOptions: {
-        radialBar: {
-          hollow: {
-            margin: 0,
-            size: "70%",
-            background: "#293450"
-          },
-          track: {
-            dropShadow: {
-              enabled: true,
-              top: 2,
-              left: 0,
-              blur: 4,
-              opacity: 0.15
-            }
-          },
-          dataLabels: {
-            name: {
-              offsetY: -10,
-              color: "#fff",
-              fontSize: "13px"
-            },
-            value: {
-              color: "#fff",
-              fontSize: "30px",
-              show: true
-            }
-          }
+        value: {
+          color: "#fff",
+          fontSize: "30px",
+          show: true
         }
-      },
-      fill: {
-        type: "gradient",
-        gradient: {
-          shade: "dark",
-          type: "vertical",
-          gradientToColors: ["#87D4F9"],
-          stops: [0, 100]
-        }
-      },
-      stroke: {
-        lineCap: "round"
-      },
-      labels: ["Transportation"]
-    };
-    
-    var chart2 = new ApexCharts(document.querySelector("#chart2"), options);
-    
-    chart2.render();
+      }
+    }
+  },
+  fill: {
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      type: "vertical",
+      gradientToColors: ["#87D4F9"],
+      stops: [0, 100]
+    }
+  },
+  stroke: {
+    lineCap: "round"
+  },
+  labels: ["Transportation"]
+};
+
+var chart2 = new ApexCharts(document.querySelector("#chart2"), options);
+
+chart2.render();
