1c7ff1187ab624c25c7c98ad2d12690f825b9555
fioriecheyenne <fioriecheyenne@gmail.com>
2019-06-05 21:26:45 -0700

    working expenses object

--- public/js/table.js
+++ public/js/table.js
@@ -1,14 +1,56 @@
 // expense page javascript
+// console.log(window.location.search);
 
-//Add additional rows for expenses 
-$("#add").on("click", () => {
-    const newRow = $(".erow");
-    newRow.clone().prependTo(".inputbuttons").removeClass("erow");
-     newRow.clone().appendTo(".is-half").removeClass("erow");
-    }
-  );
-  
-  //Redirects user to their budget breakdown page once expenses are added 
-  $("#budgetbreak").on("click", () => {
-    window.location.href="./userbreakdown.html"
+//getting ID from the window
+let params = {};
+window.location.search
+  .split(/[\?&]/)
+  .filter(s => s !== "")
+  .forEach(s => {
+    let [key, value] = s.split("=");
+    params[key] = value;
   });
+console.log(params);
+
+const { userID } = params;
+console.log(userID);
+
+//Add additional rows for expenses
+$("#add").on("click", () => {
+  const newRow = $(".erow");
+  console.log(newRow);
+
+  newRow.clone()
+    .prependTo(".inputbuttons")
+    .removeClass("erow").children().children().val("");
+  newRow
+    .clone()
+    .appendTo(".is-half")
+    .removeClass("erow").children().children().val("");
+});
+
+
+
+//Redirects user to their budget breakdown page once expenses are added
+$("#budgetbreak").on("click", () => {
+  // window.location.href="./userbreakdown.html"
+  let expenses = [
+    // {
+    //   name: '',
+    //   price: '',
+    //   category: ''
+    // }
+  ];
+  $(".expense-row").each((i, element) => {
+    var expense = {}
+    // grab all three values
+    expense.name = element.children[0].children[0].value;
+    // put them into an object
+    expense.value = element.children[1].children[0].value;
+    expense.category = element.children[2].children[0].children[0].value;
+
+    // push the object into the expenses array
+    expenses.push(expense);
+    console.log(expense);
+  });
+});
